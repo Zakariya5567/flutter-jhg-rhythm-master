7c56ef12785a258d0d8dbd7b988743d56da572bb
Muhammad zakariya <mdzakariya333@gmail.com>
2024-03-13 17:25:16 +0500

    metronome sound silent on 6/8 done

--- lib/providers/metro_provider.dart
+++ lib/providers/metro_provider.dart
@@ -437,9 +437,9 @@ class MetroProvider extends ChangeNotifier {
     totalTick = totalTick + 1;
     if (totalTick == 1) {
       if (player.playing) {
-        await player.stop();
-        await player.setAsset(firstBeat);
-        await player.play();
+         player.stop();
+         player.setAsset(firstBeat);
+         player.play();
       } else {
         await player.setAsset(firstBeat);
         await player.play();
@@ -447,14 +447,14 @@ class MetroProvider extends ChangeNotifier {
     } else {
       if (totalTick < totalBeat + 1) {
         if (player.playing) {
-          await player.stop();
-          await player.setAsset(secondBeat);
-          await player.play();
+          player.stop();
+          player.setAsset(secondBeat);
+          player.play();
         } else {
           try {
             print("playing==");
-            await player.setAsset(secondBeat);
-            await player.play();
+             player.setAsset(secondBeat);
+             player.play();
           } catch (e) {
             print("==${e}");
           }
