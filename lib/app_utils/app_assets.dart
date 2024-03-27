95079f45191aa45c7ed01ec74cf8f1965f0e7565
Faizan Akram <48491556+faizanfiverr909@users.noreply.github.com>
2024-03-28 00:38:24 +0500

    removed unused resources

--- lib/app_utils/app_assets.dart
+++ lib/app_utils/app_assets.dart
@@ -1,9 +1,7 @@
 class AppAssets {
-  static const iconReset = "assets/images/ic_reset.png";
   static const arrowDown = "assets/images/arrow_down.png";
   static const metronome = "assets/images/metronome.png";
   static const slider = "assets/images/slider.png";
   static const stalk = "assets/images/stalk.png";
   static const metronomeBottom = "assets/images/metronome_bottom.png";
-  static const metronomeBack = "assets/images/metronome_back.png";
 }
