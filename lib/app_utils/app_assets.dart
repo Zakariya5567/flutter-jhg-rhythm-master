a3392979dfa9a1b7404a963242d0f503abd47738
Faizan Akram <48491556+faizanfiverr909@users.noreply.github.com>
2024-05-23 23:27:23 +0500

    fixed the metronome image issue for web

--- lib/app_utils/app_assets.dart
+++ lib/app_utils/app_assets.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/foundation.dart';
+
 class AppAssets {
-  static const arrowDown = "assets/images/arrow_down.png";
-  static const metronome = "assets/images/metronome.png";
-  static const slider = "assets/images/slider.png";
-  static const stalk = "assets/images/stalk.png";
-  static const metronomeBottom = "assets/images/metronome_bottom.png";
+  static const arrowDown =
+      "${kIsWeb ? 'web/' : ''}assets/images/arrow_down.png";
+  static const metronome = "${kIsWeb ? 'web/' : ''}assets/images/metronome.png";
+  static const slider = "${kIsWeb ? 'web/' : ''}assets/images/slider.png";
+  static const stalk = "${kIsWeb ? 'web/' : ''}assets/images/stalk.png";
+  static const metronomeBottom =
+      "${kIsWeb ? 'web/' : ''}assets/images/metronome_bottom.png";
 }
