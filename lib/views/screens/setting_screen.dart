f29708c091d171e17f434a312e71dd9a7b3662fe
Kamran <75194058+Kamisofts@users.noreply.github.com>
2024-09-26 18:08:54 +0500

    error fixed

--- lib/views/screens/setting_screen.dart
+++ lib/views/screens/setting_screen.dart
@@ -69,17 +69,16 @@ class _SettingScreenState extends State<SettingScreen> {
         return Container(
           height: MediaQuery.sizeOf(context).height / 1.4,
           color: AppColors.blackPrimary,
-          child: Expanded(
-              child: Container(
-            // constraints: BoxConstraints(maxWidth: 345),
-            child: homeProvider.selectedButton == 0
-                ? MetronomeSetting(controller: controller)
-                : homeProvider.selectedButton == 1
-                    ? TapTempoSetting(controller: controller)
-                    : SpeedTrainerSetting(
-                        controller: controller,
-                        speedController: speedController!),
-          ).center.paddingOnly(top: height * 0.02)),
+          child: Container(
+                      // constraints: BoxConstraints(maxWidth: 345),
+                      child: homeProvider.selectedButton == 0
+            ? MetronomeSetting(controller: controller)
+            : homeProvider.selectedButton == 1
+                ? TapTempoSetting(controller: controller)
+                : SpeedTrainerSetting(
+                    controller: controller,
+                    speedController: speedController!),
+                    ).center.paddingOnly(top: height * 0.02),
         );
       }),
       trailing: isFreePlan
